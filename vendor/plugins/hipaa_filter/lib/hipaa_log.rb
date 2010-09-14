e102a5df2ce624d4551b73bf8b2712c8bcf3be9d
Max Warnock <circuitshaman@gmail.com>
2010-09-14 09:17:45 -0400

    Removed Unneccessary Code from Hipaa Filter Plugin
    Removed references to the rest-open-uri gem as well as the analytics
    filter which is unused.

--- vendor/plugins/hipaa_filter/lib/hipaa_log.rb
+++ vendor/plugins/hipaa_filter/lib/hipaa_log.rb
@@ -1,5 +1,5 @@
 # HipaaLog
-require 'rest-open-uri'
+
 module HipaaLog
 
   module ControllerFilters
@@ -8,48 +8,6 @@ module HipaaLog
       after_filter HipaaLog::PHIFilter, *filters
     end
 
-    def analytics_filter(*filters, &block)
-      after_filter HipaaLog::AnalyticsFilter, *filters
-    end
-
-  end
-
-  class AnalyticsFilter
-
-    attr_accessor :context, :view_log_hash
-
-    #Class method used by after_filter
-    def self.filter(controller)
-      self.new(controller)
-    end
-
-    def initialize(controller_context)
-      self.context = controller_context
-      context.session[:username] ? @username = context.session[:username] : @username = "anonymous"
-      self.view_log_hash = {:requesting_username => @username,
-        :requesting_ip => context.request.remote_ip,
-        :application_name => (context.request.host.include?(".radiology.umm.edu") ? context.request.host.split(".")[0] : context.request.host),
-        :request_uri => context.request.request_uri,
-        :created_at => Time.now.to_s(:db)
-      }
-      begin
-        open("http://radsom-metal1.radiology.umm.edu:8080/insert", :method => :post, :body => self.to_insert)
-      rescue
-        nil
-      end
-    end
-
-    def to_insert
-      insert_statment = "INSERT INTO requests ("
-      insert_statment += self.view_log_hash.keys.join(", ")
-      insert_statment += ") VALUES (\""
-      insert_statment += (self.view_log_hash.values.collect {|v| self.sanitize_value(v) }).join("\",\"")
-      insert_statment += "\")"
-    end
-
-    def sanitize_value(value)
-      value.gsub(/\\/, '\&\&').gsub(/'/, "''")
-    end
   end
 
   class PHIFilter
