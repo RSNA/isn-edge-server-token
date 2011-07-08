9c56b6bf9d324b96783cf75924e983ca34ab53b0
Max Warnock <mwarnock@umm.edu>
2011-07-08 13:10:19 -0400

    Update Log Locations
    The tail controller now has the correct location for the log files for
    the token-app, prepare content app, and transfer content app.

--- app/controllers/tail_controller.rb
+++ app/controllers/tail_controller.rb
@@ -10,14 +10,16 @@ class TailController < ApplicationController
   # Tails the last 200 lines of the concatenated edge server log
   def index
     if params[:log] == "prepare-content"
-      file = "prep-content.log"
+      file = File.join([`echo $RSNA_ROOT`,"logs","prep-content.log"])
+    elsif params[:log] == "transfer-content"
+      file = File.join(["`echo $RSNA_ROOT`","logs","transfer-content.log"])
     else
-      file = "transfer-content.log"
-      params[:log] = "transfer-content"
+      file = File.join([RAILS_ROOT,"log",RAILS_ENV + ".log"])
+      params[:log] = "token-app"
     end
 
-    @log = `tail -n 200 /rsna/logs/#{file}` if File.exists?("/rsna/logs/#{file}")
-    @log ||= "The log file is empty"
+    @log = `tail -n 200 #{file}` if File.exists?(file)
+    @log ||= "The log file (#{file}) is empty"
   end
 
 end
