1e6bf128da9cdddc086bb1eeef528dc4e39d3e17
Max Warnock <mwarnock@umm.edu>
2011-08-26 12:46:44 -0400

    Changed requires
    Moved both the digest/sha1 and digest/sha2 requires into the environment
    and removed them from the user and job_set models.

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  require 'digest'
+  require 'digest/sha2'
+  require 'digest/sha1'
 
 end
