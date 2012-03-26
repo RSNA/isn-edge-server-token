47c2661e5c0f2bb1b8672b7dc0cdff0303ba8106
Josh Stafford <jstafford1@umm.edu>
2012-03-26 11:42:54 -0400

    retrieve content app downloads

---
+++ app/controllers/retrieve_content_controller.rb
@@ -0,0 +1,27 @@
+=begin rdoc
+=Description
+Handles the requests for most administrative tasks.
+
+=Filters
+All methods are filtered using the following methods
+  super_authenticate # => ensures that the user is a super user or admin
+  get_cart # => populates the cart continuation so that the selected patient isn't lost
+  hipaa_filter # => Tracks all hipaa data
+=end
+class RetrieveContentController < ApplicationController
+  before_filter :authenticate
+
+  def index
+  end
+
+  def send_zip
+    @retrieve = File.join([`echo $RSNA_ROOT`.strip,"client_downloads","retrieve-content.zip"])
+    send_file(@retrieve)
+  end
+
+  def send_key
+    @truststore = File.join([`echo $RSNA_ROOT`.strip,"client_downloads","truststore.jks"])
+    send_file(@truststore)
+  end
+
+end
