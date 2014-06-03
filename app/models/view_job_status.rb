5fe2510015574eb93410f952201429739a76871b
Max Warnock <mwarnock@analytical.info>
2014-06-03 01:55:01 -0400

    Audit filtering by job status
    Added a filter for job status in the audit logs.

--- app/models/view_job_status.rb
+++ app/models/view_job_status.rb
@@ -6,7 +6,9 @@ class ViewJobStatus < ActiveRecord::Base
 
   # Filters using a basic search methodology on mrn, accession number, and patient name, single_user_patient_id.
   def self.filter(string)
-    self.where("patients.mrn ~* ? OR patients.patient_name ~* ? OR exams.accession_number ~* ? OR job_sets.single_use_patient_id ~* ?", string, string, string, string).joins("LEFT JOIN (jobs JOIN exams ON (jobs.exam_id = exams.exam_id)) ON (jobs.job_id = v_job_status.job_id) LEFT JOIN (job_sets JOIN patients ON (patients.patient_id = job_sets.patient_id)) ON job_sets.job_set_id = jobs.job_set_id").order("last_transaction_timestamp DESC").limit(100)
+    query = self.joins("LEFT JOIN (jobs JOIN exams ON (jobs.exam_id = exams.exam_id)) ON (jobs.job_id = v_job_status.job_id) LEFT JOIN (job_sets JOIN patients ON (patients.patient_id = job_sets.patient_id)) ON job_sets.job_set_id = jobs.job_set_id").order("last_transaction_timestamp DESC").limit(100)
+    query = query.where(*Search::Query.new(string).conditions()) unless string.blank?
+    query
   end
 
 end
