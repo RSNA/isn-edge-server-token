8d9b34c29b86319e367211a6c34b355451a51fad
Wayne LaBelle <wlabelle@umm.edu>
2014-11-13 16:19:41 -0500

    Fix localhost static openam url

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -70,7 +70,7 @@ module AuthenticatedSystem
       respond_to do |format|
         format.html do
           store_location
-          redirect_to "http://localhost:3000/openam/UI/Login"
+          redirect_to ENV["OPENAM_URL"] + "/UI/Login"
         end
         # format.any doesn't work in rails version < http://dev.rubyonrails.org/changeset/8987
         # Add any other API formats here.  (Some browsers, notably IE6, send Accept: */* and trigger
