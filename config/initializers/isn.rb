38fd60753f555b0e7f9561a4b0f6b493bf5564ec
Wayne LaBelle <wlabelle@umm.edu>
2015-01-02 13:19:56 -0500

    Fix OpenAM startup guarding in token-app

--- config/initializers/isn.rb
+++ config/initializers/isn.rb
@@ -1,3 +1,33 @@
+require 'net/http'
+
 if !ENV['ISN_BUILD']
   Java::org.rsna.isn.util.Environment.init("token")
+end
+if ENV["RAILS_ENV"] == "production"
+  logger.info "Waiting for OpenAM startup #{openam_url}"
+
+  openam_url = URI(ENV["OPENAM_URL"])
+  tries = 0
+  timeout = 8
+  openam_ready = false
+
+  while !openam_ready and (tries < timeout) do
+    res = Net::HTTP.get_response(openam_url)
+    if res.code == 404
+      tries+=1
+      sleep((2.0 ** tries) - 1.0)
+    else
+      openam_ready = true
+      logger.info "OpenAM started"
+    end
+  end
+  if openam_ready
+    logger.info "Priming OpenAM agent"
+    agent_url = URI(ENV["OPENAM_URL"])
+    agent_url.path = "/agentapp/"
+    Net::HTTP.get_response(openam_url).body
+  else
+    logger.error "OpenAM startup wait timeout"
+  end
+
 end
