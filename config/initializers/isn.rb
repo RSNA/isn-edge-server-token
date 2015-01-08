c3dedfe6a9178902a3df18c7c152e89fd9f8ce4e
Wayne LaBelle <wlabelle@umm.edu>
2015-01-08 14:28:28 -0500

    Fix requires for sso in initializers

--- config/initializers/isn.rb
+++ config/initializers/isn.rb
@@ -1,3 +1,5 @@
+require 'sso'
+
 if !ENV['ISN_BUILD']
   Java::org.rsna.isn.util.Environment.init("token")
 end
--- config/initializers/restful-authentication.rb
+++
@@ -1,2 +0,0 @@
-require 'restful-authentication/rails/init'
-require 'authenticated_system'
