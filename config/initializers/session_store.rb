66a08408ef58767690a8947a91c81b50bf4fcaae
Wayne LaBelle <wlabelle@umm.edu>
2015-08-19 09:23:21 -0400

    Revert "Change session store"
    This reverts commit 162291f4eef203f2b85db270e55149c3b247dfed.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Configure the TorqueBox Servlet-based session store.
 # Provides for server-based, in-memory, cluster-compatible sessions
 if ENV['TORQUEBOX_APP_NAME']
-  TokenApp::Application.config.session_store :cache_store
+  TokenApp::Application.config.session_store :torquebox_store
 else
   TokenApp::Application.config.session_store :cookie_store, :key => '_isn_token_app_session'
 end  
