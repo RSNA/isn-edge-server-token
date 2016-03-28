ad7ebdb3c447e34fe1e7e2b5a8b367d44ab3a19b
Wayne LaBelle <wlabelle@umm.edu>
2016-03-28 09:29:03 -0400

    Fix issue with a user going directly to an admin/super url without being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,13 +49,13 @@ class ApplicationController < ActionController::Base
 
   # Check if the user is a super user
   def super_authenticate
-    authenticate if @logged_in.nil?
+    return authenticate if @logged_in.nil?
     unauthorized if !(@sso_groups.include?("Super") || @sso_groups.include?("Admin"))
   end
 
   # Check if the user is and administrator
   def admin_authenticate
-    authenticate if @logged_in.nil?
+    return authenticate if @logged_in.nil?
     unauthorized if !@sso_groups.include?("Admin")
   end
 
